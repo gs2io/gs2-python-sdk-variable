19c8cb77de42016e4172a436f3b53f504ab686eb
Kazutomo Niwa <niwa_kazutomo@gs2.io>
2018-07-13 17:50:12 +0900

    ライブラリのバージョンを更新

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='gs2-python-sdk-variable',
-    version='1.1.9',
+    version='1.1.10',
     package_dir={'': 'src'},
     packages=[
         "",
@@ -14,6 +14,10 @@ setup(
         "gs2_variable_client.control",
         "gs2_variable_client.model",
     ],
+    install_requires=[
+        'gs2-python-sdk-core >= 1.1.10',
+    ],
+    tests_require=[],
     license='Apache License 2.0',
     description='GS2-Variable SDK for Python.',
     url='https://gs2.io/',
